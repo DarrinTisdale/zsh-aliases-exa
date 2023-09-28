56fcdf49a9cc2bb7db06a7acd8a960bba779ca16
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-28 14:03:09 +0100

    Remove git support from exa

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -5,16 +5,16 @@
 # Author:   Darrin Tisdale
 # -----
 # Modified: 2019-05-14 23:18:24
-# Editor:   Darrin Tisdale
+# Editor:   Manoj Manivannan
 #
 
 # general use
 alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias l='exa -lbF'                                               # list, size, type, git
+alias ll='exa -lbGF'                                             # long list
+alias llm='exa -lbGF --sort=modified'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --color-scale' # all + extended list
 
 # speciality views
 alias lS='exa -1'			                                                  # one column, just names
